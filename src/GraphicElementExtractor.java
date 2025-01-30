5629783caad56e74f81226d28362a06611c94c74
marcocapote <marcocapote15@gmail.com>
2025-01-30 11:28:15 -0300

    getFonts agora retorna o espaçamento de cores real (retornava apenas DeviceGray)

---
+++ src/GraphicElementExtractor.java
@@ -0,0 +1,128 @@
+import org.apache.pdfbox.contentstream.PDFGraphicsStreamEngine;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImage;
+import org.apache.pdfbox.pdmodel.graphics.state.PDGraphicsState;
+import org.apache.pdfbox.util.Matrix;
+import org.apache.pdfbox.util.Vector;
+
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GraphicElementExtractor extends PDFGraphicsStreamEngine {
+    private final PDPage currentPage;
+    private final PDDocument document;
+
+    public GraphicElementExtractor(PDPage page, PDDocument document) {
+        super(page);
+        this.currentPage = page;
+        this.document = document;
+    }
+
+    @Override
+    public void drawImage(PDImage pdImage) throws IOException {
+        System.out.println("Image detected on page: " + getPageNumber(currentPage));
+        System.out.println("  - Width: " + pdImage.getWidth());
+        System.out.println("  - Height: " + pdImage.getHeight());
+        System.out.println("  - Color Space: " + pdImage.getColorSpace().getName());
+    }
+
+    @Override
+    public void clip(int i) throws IOException {
+
+    }
+
+    @Override
+    public void fillPath(int windingRule) throws IOException {
+        PDGraphicsState state = getGraphicsState();
+        PDColor fillColor = state.getNonStrokingColor();
+        PDColorSpace fillColorSpace = state.getNonStrokingColorSpace();
+        System.out.println("Fill Path detected on page: " + getPageNumber(currentPage));
+        System.out.println("  - Fill Color: " + fillColor);
+        System.out.println("  - Fill Color Space: " + fillColorSpace);
+    }
+
+    @Override
+    public void fillAndStrokePath(int i) throws IOException {
+
+    }
+
+    @Override
+    public void shadingFill(COSName cosName) throws IOException {
+
+    }
+
+    @Override
+    public void strokePath() throws IOException {
+        PDGraphicsState state = getGraphicsState();
+        PDColor strokeColor = state.getStrokingColor();
+        PDColorSpace strokeColorSpace = state.getStrokingColorSpace();
+        System.out.println("Stroke Path detected on page: " + getPageNumber(currentPage));
+        System.out.println("  - Stroke Color: " + strokeColor);
+        System.out.println("  - Stroke Color Space: " + strokeColorSpace);
+    }
+
+    private int getPageNumber(PDPage page) {
+        PDPageTree pages = document.getDocumentCatalog().getPages();
+        return pages.indexOf(page) + 1;
+    }
+
+    public void extractElements(File file, int PAGE_LIMIT){
+        try (PDDocument document = PDDocument.load(file)) {
+            PDPageTree pages = document.getDocumentCatalog().getPages();
+            Scanner scanner = new Scanner(System.in);
+            int currentIndex = 0;
+
+            while (currentIndex < pages.getCount()) {
+                int endIndex = Math.min(currentIndex + PAGE_LIMIT, pages.getCount());
+
+                for (int i = currentIndex; i < endIndex; i++) {
+                    PDPage page = pages.get(i);
+                    GraphicElementExtractor extractor = new GraphicElementExtractor(page, document);
+                    extractor.processPage(page);
+                }
+
+                currentIndex = endIndex;
+
+                if (currentIndex < pages.getCount()) {
+                    System.out.println("Digite 'next' para processar as próximas " + PAGE_LIMIT + " páginas ou 'exit' para sair:");
+                    String input = scanner.nextLine().trim().toLowerCase();
+
+                    if ("exit".equals(input)) {
+                        break;
+                    } else if (!"next".equals(input)) {
+                        System.out.println("Comando inválido. Use 'next' para continuar ou 'exit' para sair.");
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            System.out.println("Processamento concluído.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void moveTo(float x, float y) throws IOException {}
+    @Override
+    public void lineTo(float x, float y) throws IOException {}
+    @Override
+    public void curveTo(float x1, float y1, float x2, float y2, float x3, float y3) throws IOException {}
+    @Override
+    public Point2D getCurrentPoint() throws IOException { return new Point2D.Float(0, 0); }
+    @Override
+    public void closePath() throws IOException {}
+    @Override
+    public void endPath() throws IOException {}
+    @Override
+    public void appendRectangle(Point2D p0, Point2D p1, Point2D p2, Point2D p3) throws IOException {}
+}
---
+++ src/getFonts.java
@@ -0,0 +1,75 @@
+import org.apache.pdfbox.contentstream.PDFStreamEngine;
+import org.apache.pdfbox.contentstream.operator.Operator;
+import org.apache.pdfbox.contentstream.operator.color.*;
+import org.apache.pdfbox.contentstream.operator.text.ShowText;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
+import org.apache.pdfbox.pdmodel.graphics.state.PDGraphicsState;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class getFonts extends PDFStreamEngine {
+
+    private int currentPage;
+
+    public getFonts() throws IOException {
+        addOperator(new ShowText());
+        // Add color operators
+        addOperator(new SetStrokingColorSpace());
+        addOperator(new SetNonStrokingColorSpace());
+        addOperator(new SetStrokingColor());
+        addOperator(new SetNonStrokingColor());
+        addOperator(new SetStrokingColorN());
+        addOperator(new SetNonStrokingColorN());
+        addOperator(new SetStrokingDeviceGrayColor());
+        addOperator(new SetNonStrokingDeviceGrayColor());
+        addOperator(new SetStrokingDeviceRGBColor());
+        addOperator(new SetNonStrokingDeviceRGBColor());
+        addOperator(new SetStrokingDeviceCMYKColor());
+        addOperator(new SetNonStrokingDeviceCMYKColor());
+    }
+
+    public void extractFonts(PDDocument document) throws IOException {
+        int pageIndex = 1;
+        for (PDPage page : document.getPages()) {
+            currentPage = pageIndex;
+            processPage(page);
+            pageIndex++;
+        }
+    }
+
+    @Override
+    protected void processOperator(Operator operator, List<COSBase> operands) throws IOException {
+        String operation = operator.getName();
+
+        if ("Tj".equals(operation) || "TJ".equals(operation)) { // Operators that draw text
+            PDGraphicsState state = getGraphicsState();
+            PDColor color = state != null ? state.getNonStrokingColor() : null;
+            PDResources resources = getResources();
+
+            if (resources != null) {
+                List<String> fontes = new ArrayList<>();
+
+                for (COSName nomeFonte : resources.getFontNames()) {
+                    PDFont font = resources.getFont(nomeFonte);
+                    if (font != null) {
+                        fontes.add(font.getName()); // Get the real font name
+                    }
+                }
+
+                String info = "Fonte(s): " + fontes + " | Cor: " + (color != null ? color.getColorSpace().getName()  + Arrays.toString(color.getComponents()) : "Indefinido ");
+                System.out.println("Página: " + currentPage + " - " + info);
+            }
+        }
+        super.processOperator(operator, operands);
+    }
+
+
+}
